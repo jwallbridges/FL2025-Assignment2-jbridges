c3ba81622e566f24e29d69a57dafa6fb15d960ef
walli <walli@JWB>
2025-10-01 23:16:32 -0500

    Add project files.

---
+++ FL2025-Assignment2-jbridges/Program.cs
@@ -0,0 +1,16 @@
+namespace FL2025_Assignment2_jbridges
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run();
+        }
+    }
+}
